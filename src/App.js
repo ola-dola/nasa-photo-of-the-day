6ddb5737ec6d7287ec2256d645482219dce85224
oredola <oredolaolamide@gmail.com>
2019-11-06 14:46:23 +0100

    Data fetched from API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 import "./App.css";
 
 function App() {
+  const [nasaData, setNasaData] = useState({});
+
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=QEjorSwWWs988U9L7I0W2XtYuiqAaI9qB9RpUzy8')
+    .then(response => {
+      console.log(response.data);
+      // setNasaData(response.data);
+    })
+    .catch(error => {
+      console.log(error.message);
+    })
+  }, []);
+
   return (
     <div className="App">
       <p>
@@ -12,4 +26,5 @@ function App() {
   );
 }
 
+{/* <p>🚀</p> */}
 export default App;
